0a908d6a0d58e5649289b8b2d8a4079d1e5085fe
Anwar Said <anwar.said@itu.edu.pk>
2023-07-17 09:32:27 -0500

    test workflow updated

--- test.py
+++ test.py
@@ -5,17 +5,19 @@ import csv
 import sys
 
 test_data = sys.argv[1]
+trained_model = sys.argv[2]
 # test_data = "data/test_data.pt"
 num_layers = 3
 hidden = 64
 num_classes= 2
 hidden_mlp = 64
 num_features = 1000
-
-model = ResidualGNNs(num_features,hidden,hidden_mlp,num_layers, num_classes)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = "cpu"
+model = ResidualGNNs(num_features,hidden,hidden_mlp,num_layers, num_classes).to(device)
 print(model)
 
-model.load_state_dict(torch.load("data/gcn_trained.pkl"))
+model.load_state_dict(torch.load(trained_model,map_location=torch.device('cpu')))
 
 test_dataset = torch.load(test_data)
 test_loader = DataLoader(test_dataset, 1, shuffle= False)
@@ -24,7 +26,7 @@ def test(loader):
     model.eval()
     predictions = []
     for data in loader:  
-        data = data
+        data = data.to(device)
         out = model(data)  
         pred = out.argmax(dim=1)  
         predictions.append(pred.item())
